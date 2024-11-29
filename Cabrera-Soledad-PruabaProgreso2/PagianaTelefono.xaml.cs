aea75b95fe5c94df9ffdb1d97b1c630652bfa9dc
Sol8888 <daniela.cabrera.tapia@udla.edu.ec>
2024-11-29 07:37:12 -0500

    Cambios en el xaml de PaginaTelfono para guardar las cosas en un archivo. (ojala funcione)

--- Cabrera-Soledad-PruabaProgreso2/PagianaTelefono.xaml.cs
+++ Cabrera-Soledad-PruabaProgreso2/PagianaTelefono.xaml.cs
@@ -1,14 +1,39 @@
+using System;
+using System.IO;
+using Microsoft.Maui.Controls;
+
 namespace Cabrera_Soledad_PruabaProgreso2;
 
 public partial class PagianaTelefono : ContentPage
 {
-	public PagianaTelefono()
+    public string UltimaRecarga { get; set; }
+
+    public PagianaTelefono()
 	{
 		InitializeComponent();
-	}
+        BindingContext = this;
+        
+    }
 
     private void scabrera_buttonRecargar_Clicked(object sender, EventArgs e)
     {
+        var nombre = scabrera_entryNombre.Text;
+        var telefono = scabrera_entryTelefono.Text;
 
+        if (string.IsNullOrWhiteSpace(nombre) || string.IsNullOrWhiteSpace(telefono))
+        {
+            DisplayAlert("Error", "Por favor ingrese todos los datos.", "Okey");
+            return;
+        }
+
+        var datos = $"Nombre: {nombre}, Teléfono: {telefono}, Fecha: {DateTime.Now}";
+        var filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "SoledadCabrera.txt");
+
+        File.WriteAllText(filePath, datos);
+        DisplayAlert("Éxito", "Recarga realizada correctamente.", "OK");
+
+        
     }
+    
+    
 }
